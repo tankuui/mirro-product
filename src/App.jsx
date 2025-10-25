f9601ed36facd03f13d18a541f76c44a574fc304
tankuui <98266982+tankuui@users.noreply.github.com>
2025-10-25 16:30:04 +0800

    Added index.html

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'system-ui, sans-serif' }}>
+      <h1>Welcome</h1>
+      <p>Your app is running successfully!</p>
+    </div>
+  );
+}
+
+export default App;
